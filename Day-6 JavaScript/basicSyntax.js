ed1cb2816bd3ea40c9f83ad452b2e72b16a52261
Arelvee <18-01098@g.batstate-u.edu.ph>
2024-04-23 13:30:00 +0800

    added JS basic

---
+++ Day-6 JavaScript/basicSyntax.js
@@ -0,0 +1,27 @@
+// Variables
+// let - mutable or changeable variable
+let myName = "Abbi";
+console.log(myName);
+
+
+myName = "Isabel";
+console.log(myName);
+
+
+// const - unchangeable variable
+const myYear = 2024;
+console.log(myYear);
+
+
+
+// myYear = 2025;
+// console.log(myYear); - will give an error
+// var
+
+
+var myHobby = "sleeping";
+console.log(myHobby);
+
+
+myHobby = "eating";
+console.log(myHobby);
