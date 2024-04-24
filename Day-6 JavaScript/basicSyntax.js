555d10b436f45acdb6e6ec92abc04cc7bcc316ed
Arelvee <18-01098@g.batstate-u.edu.ph>
2024-04-24 10:54:31 +0800

    add arrays lesson

--- Day-6 JavaScript/basicSyntax.js
+++ Day-6 JavaScript/basicSyntax.js
@@ -25,3 +25,46 @@ console.log(myHobby);
 
 myHobby = "eating";
 console.log(myHobby);
+
+//console.log(MyHobby);  --different from myHobby
+
+// console.log(MyHobby); - different from myHobby
+// Key Points
+// Comments
+// Case sensitivity - camelCase
+// White Spaces
+
+// Data Types - Primitive Types
+// Dynamic Typing
+// Number -
+let number = 13;
+number = 13.34;
+number = -12;
+console.log("Number: ", number);
+// String - texts '' or ""
+let word = "Hello, world!";
+// hello = 'Hello Javascript's world' - best practice to use double quotes
+console.log("String: ", word);
+// Boolean - true or false
+let status = true;
+status = false;
+console.log("Boolean: ", status);
+// Undefined - uninitialized or missing value
+let nothing;
+console.log(nothing);
+// Null
+let nullVar = null;
+console.log(nullVar);
+// Reference Types
+// Object - key-value pair
+// Array
+// Function
+// Date
+
+
+
+
+
+
+
+
