dbac99294102f4659198ca9d9ef10a252c506189
Arelvee <arelvisaya@gmail.com>
2024-05-02 21:30:50 +0800

    added qr code scan

---
+++ Day-14 QR-Generator/qrCode.js
@@ -0,0 +1,34 @@
+// Import dependencies and setup express
+const express = require("express");
+const QRCode = require("qrcode");
+const app = express();
+const PORT = 3000;
+// Define QR Generation route
+app.get("/qrcode", (req, res) => {
+  const url = "https://google.com";
+  QRCode.toDataURL(url, (err, qrCodeUrl) => {
+    if (err) {
+      // internal server error
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.send(`
+      <!DOCTYPE HTML>
+      <html>
+        <head>
+          <title>QR Code Generator</title>
+        </head>
+        <body>
+          <h1>QR Code Generator</h1>
+          <img src="${qrCodeUrl}" alt="QR Code">
+          <p>Scan the QR Code to visit the website</p>
+        </body>
+      </html>
+      `);
+    }
+  });
+});
+
+// Start the server and listen to request
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
