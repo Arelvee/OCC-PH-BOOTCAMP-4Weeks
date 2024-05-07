1fa6b74203631a8aee8896dfba7264d08c15df81
Arelvee <arelvisaya@gmail.com>
2024-05-07 18:27:49 +0800

    added restful api

---
+++ Day-17 Restful API/src/model/post.js
@@ -0,0 +1,15 @@
+const { MongoTopologyClosedError } = require("mongodb");
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    description: { type: String, required: true },
+    likes: { type: Number },
+    comments: [{ body: String }],
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+
+module.exports = Post;
